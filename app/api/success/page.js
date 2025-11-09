155b28cb09770c481e635c271a2d2fb3348d4d31
Abhijit Patra <abhijit.patra224@gmail.com>
2025-11-09 13:19:53 +0530

    commented success page

--- app/api/success/page.js
+++ app/api/success/page.js
@@ -3,25 +3,25 @@ import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function SuccessPage() {
-  const searchParams = useSearchParams();
+//   const searchParams = useSearchParams();
   const [status, setStatus] = useState("Verifying...");
 
-  useEffect(() => {
-    const txnId = searchParams.get("transactionId");
-    if (!txnId) return;
+//   useEffect(() => {
+//     const txnId = searchParams.get("transactionId");
+//     if (!txnId) return;
 
-    fetch("/api/verifyPayment", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ merchantTransactionId: txnId }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status) setStatus(`Payment ${data.status}`);
-        else setStatus("Payment verification failed");
-      })
-      .catch(() => setStatus("Error verifying payment"));
-  }, []);s
+//     fetch("/api/verifyPayment", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({ merchantTransactionId: txnId }),
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         if (data.status) setStatus(`Payment ${data.status}`);
+//         else setStatus("Payment verification failed");
+//       })
+//       .catch(() => setStatus("Error verifying payment"));
+//   }, []);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
